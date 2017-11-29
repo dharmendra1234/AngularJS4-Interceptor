8cf543719c9ccbd478b72b4d564a8a0f95576cf5
dharmendra1234 <dharmendra.pawar@gmail.com>
2017-11-29 17:53:17 -0500

    updated

--- src/app/userservice.ts
+++ src/app/userservice.ts
@@ -1,8 +1,8 @@
-import {Injectable} from '@angular/core';
-import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest,HttpHeaders} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpHeaders } from '@angular/common/http';
 
-import {Http, Headers, Response,RequestOptions} from "@angular/http";
-import {Observable} from "rxjs";
+import { Http, Headers, Response, RequestOptions } from "@angular/http";
+import { Observable } from "rxjs";
 import "rxjs/add/operator/map";
 
 import 'rxjs/add/operator/map';
@@ -10,65 +10,29 @@ import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
 
 import { HttpClient } from "@angular/common/http";
+import { Token } from "app/token";
 
 @Injectable()
-export class userservice  {
-  public access_token: string;
-    public refresh_token: string;
-
-
-    private readonly LOGIN_URL: string = 'http://localhost:9966/api/auth/login';
-    private readonly REFRESH_URL: string = 'http://localhost:9966/api/auth/token';
-    private readonly ACCESS_STORAGE_KEY: string = 'accessStorage';
-    private readonly REFRESH_STORAGE_KEY: string = 'refreshStorage';
-
- 
-  private url: string = "http://jsonplaceholder.typicode.com/users";
+export class userservice {
+  results: any;
 
   constructor(private http: HttpClient) { }
 
-//   getUsers(){
-//     return this.http.get(this.url).subscribe(res=>console.log(res));
-   
-//   }
-
-  public getUsers() {
-      console.log("ssdfsdf");
-    this.http.get('http://jsonplaceholder.typicode.com/users')
-      .subscribe(
-        data => console.log(data),
-        err => console.log(err)
-      );
+  getToken() {
+    let usercrenditional = { "username": "svlada@gmail.com", "password": "test1234" };
+    let body = JSON.stringify(usercrenditional);
+    this.http.post('http://localhost:9000/api/auth/login', body)
+      .map((response: Token) => {
+        localStorage.setItem('token', 'Bearer ' + response.token);
+        localStorage.setItem('refreshToken', 'Bearer ' + response.refreshToken);
+      }).subscribe();
   }
 
-  login()  {
-   
-     let usercrenditional=  { "username": "svlada@gmail.com",   "password": "test1234"   };
-    var headers= new Headers();   headers.append('X-Requested-With','XMLHttpRequest' );    headers.append('Content-Type','application/json' );    headers.append('Cache-Control','no-cache' );
-     let options = new RequestOptions({ headers: headers });
-    let body = JSON.stringify(usercrenditional);
-  //  return this.http.post('http://localhost:9966/api/auth/login',body, {headers : headers} )
 
-  return  this.http
-  .post('http://localhost:9000/api/auth/login', body, {
-    headers: new HttpHeaders().set('X-Requested-With','XMLHttpRequest').append('Content-Type','application/json').append('Cache-Control','no-cache')
-  })
- 
-
-   .flatMap((response: Response) => {
-   //    response.json;
-                let recieved_access_token = response.json() && response.json().token;
-                if (recieved_access_token) {
-                   this.access_token = recieved_access_token;
-                    var accessDetails = JSON.parse(localStorage.getItem(this.ACCESS_STORAGE_KEY));
-                    localStorage.setItem(this.ACCESS_STORAGE_KEY, 'Bearer ' + this.access_token);
-                     return Observable.of(true);
-                } else {
-                     //  this.logout();
-                       return Observable.of(false);
-                }
-            }).subscribe( token => 
-            console.log("Token " + token) );
+  checkServicewithToekn() {
+    this.http.get('http://localhost:9999/api/greeting/').subscribe(data => {
+      this.results = data['results'];
+    });
   }
 
 }
