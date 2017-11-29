d66dc121fbec53abcbd04100098de3ec43157d4e
dharmendra1234 <dharmendra.pawar@gmail.com>
2017-11-29 17:53:47 -0500

    updated

--- src/app/NoopInterceptor.ts
+++ src/app/NoopInterceptor.ts
@@ -1,15 +1,31 @@
 import {Injectable} from '@angular/core';
 import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest} from '@angular/common/http';
 import { Observable } from "rxjs/Observable";
+import { HttpResponse } from "@angular/common/http";
+import { HttpErrorResponse } from "@angular/common/http";
 
 @Injectable()
 export class NoopInterceptor implements HttpInterceptor {
  
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let token= localStorage.getItem('accessStorage');
-    console.log('accessstorage'+token)
-    const authReq = req.clone({headers: req.headers.set('Authorization', token)});
-    return next.handle(authReq);
-    //Bearer
+
+    const authReq = req.clone({headers: req.headers.set('Authorization',  localStorage.getItem('token'))});
+
+    return next.handle(authReq)
+  // .do(events => {
+  //     if (envents instanceof HttpResponse) {
+  //       console.log('---> log the status:', events.status);
+  //    
+  //     }
+  //   })
+  .catch(response => {
+        if (response instanceof HttpErrorResponse) {
+          console.log('Processing http error', response);
+        }
+        return Observable.throw(response);
+      });
   }
 }
+
+  
+
